d49eb867df0e015da5b894a6a3ae763680814456
francescagiunchedi <39530831+francescagiunchedi@users.noreply.github.com>
2022-03-01 15:39:31 +0100

    updated java (temperature stats)

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 function showTemperature(response) {
-  console.log(response);
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.wind.speed;
 }
-let apiKey = "4ff86370e362a341be667316345b71fc";
-let apiUrl = `api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(showTemperature);
+let apiKey = "4ff86370e362a341be667316345b71fc";
+let api = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(api).then(showTemperature);
