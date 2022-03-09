c30776e590e2cfc7bd11e9097b14180c38b495be
francescagiunchedi <39530831+francescagiunchedi@users.noreply.github.com>
2022-03-09 19:06:11 +0100

    http to https

--- src/app.js
+++ src/app.js
@@ -118,7 +118,7 @@ function showLocation(position) {
   let apiKey = "4ff86370e362a341be667316345b71fc";
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
-  let api = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  let api = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&appid=${apiKey}`;
   axios.get(api).then(getCity);
 }
 
